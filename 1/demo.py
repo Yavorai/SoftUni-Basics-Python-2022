43c5ec1d0de874265aaf317b048fd7e810adb6eb
Yavor Pavlov <89733118+Yavorai@users.noreply.github.com>
2022-10-30 22:24:04 +0200

    Add files via upload

---
+++ 1/demo.py
@@ -0,0 +1,21 @@
+'''user_name = input('Enter valid username: ') # comment
+print(user_name)
+print(type(user_name))'''
+
+"""# number = int(input('Enter valid number: '))
+number = float(input('Enter valid number: ')) # 4.5
+area = number * 2
+print(area)"""
+
+'''from math import ceil, floor # вместо цялта библиотека само това което ни трябва
+
+print(ceil(3.15))
+print(floor(3.15))'''
+
+'''# QR core creater 
+import pyqrcode # трябва да се инсталира библиотека
+import png
+from pyqrcode import QRCode # трябва да се инсталира библиотека
+address = 'https://thumbs.dreamstime.com/b/bee-small-flower-macro-phoho-bee-small-flower-197645501.jpg'
+url = pyqrcode.create(address)
+url.png('bee and flower.png', scale=8)'''
---
+++ 1/pet_shop.py
@@ -0,0 +1,8 @@
+single_bag_of_food_for_dogs = float(2.50)
+single_bag_of_food_for_cats = int(4)
+
+number_of_dogs_bags = int(input())
+number_of_cats_bags = int(input())
+
+final_sum = (number_of_dogs_bags * single_bag_of_food_for_dogs) + (number_of_cats_bags * single_bag_of_food_for_cats)
+print(f"{final_sum} lv.")
