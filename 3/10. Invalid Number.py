8861ed9cd388fb2ad2339c65f046e7d5eb009935
Yavor Pavlov <89733118+Yavorai@users.noreply.github.com>
2022-11-14 00:40:24 +0200

    Update 10. Invalid Number.py

--- 3/10. Invalid Number.py
+++ 3/10. Invalid Number.py
@@ -11,3 +11,9 @@ else:
 is_valid = 100 <= number <= 200 or number == 0
 if is_valid == False:
     print('invalid')
+    
+# or
+
+is_valid = 100 <= number <= 200 or number == 0
+if not is_valid:  # само ако is_false е със стойност false проверката ще се изпълни 
+    print('invalid')
